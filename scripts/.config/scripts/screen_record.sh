63ff89f2cad578e8240f309a05abb6d10b8e7899
Maxime Gagne <max.8989@hotmail.com>
2025-03-05 23:06:10 +0800

    update script screen record

--- scripts/.config/scripts/screen_record.sh
+++ scripts/.config/scripts/screen_record.sh
@@ -11,9 +11,11 @@ sleep 1
 notify-send "Screen Recording" "Recording in: 1" -i video-display -t 2000
 sleep 1
 notify-send "Screen Recording" "Recording..." -i video-display -t 1000
+sleep 1
 
 # Set date/time for filename
 dateTime=$(date +%m-%d-%Y-%H-%M-%S)
 
 # Start recording with more explicit format settings
-wf-recorder --bframes max_b_frames -c h264_vaapi -d /dev/dri/renderD128 -f $HOME/Videos/$dateTime.mp4 2>/dev/null
+mkdir -p $HOME/Videos/ScreenRecordings
+wf-recorder --bframes max_b_frames -c h264_vaapi -d /dev/dri/renderD128 -f $HOME/Videos/ScreenRecordings/$dateTime.mp4 2>/dev/null
