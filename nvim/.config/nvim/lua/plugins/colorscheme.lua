275ce7c8546fea3e451bad4a22112fad472a3eaf
Maxime Gagne <max.8989@hotmail.com>
2025-08-13 11:45:54 -0400

    add color schemes

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup()
+      vim.cmd.colorscheme "catppuccin"
+    end
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 900,
+    config = function()
+      require("tokyonight").setup()
+      vim.cmd.colorscheme "tokyonight"
+    end
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 800,
+    config = function()
+      require("rose-pine").setup()
+      vim.cmd.colorscheme "rose-pine"
+    end
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 500,
+    config = function()
+      require("kanagawa").setup()
+      vim.cmd.colorscheme "kanagawa"
+    end
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    priority = 400,
+    config = function()
+      require("nightfox").setup()
+      vim.cmd.colorscheme "nightfox"
+    end
+  }
+}
