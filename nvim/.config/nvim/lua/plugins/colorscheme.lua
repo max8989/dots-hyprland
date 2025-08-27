0a23f7adfca3b8dad32b86eab09111f57eac2ebe
Maxime Gagne <max.8989@hotmail.com>
2025-08-26 20:06:24 -0400

    update color scheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,5 @@
 return {
-  {
-    "scottmckendry/cyberdream.nvim",
+  { "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
     config = function()
@@ -16,104 +15,20 @@ return {
             -- Custom highlights can be added here
           }
         },
+        -- Disable or enable colorscheme extensions
+        extensions = {
+          telescope = true,
+          notify = true,
+          mini = true,
+          alpha = true,
+          cmp = true,
+          gitsigns = true,
+          lazy = true,
+          lualine = true,
+          treesitter = true,
+        },
       })
       vim.cmd.colorscheme "cyberdream"
     end,
   },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 900,
-    config = function()
-      require("catppuccin").setup({
-        transparent_background = false,
-        show_end_of_buffer = false,
-        term_colors = true,
-        dim_inactive = {
-          enabled = false,
-          shade = "dark",
-          percentage = 0.15,
-        },
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          telescope = true,
-          notify = false,
-          mini = false,
-        },
-      })
-    end
-  },
-  {
-    "folke/tokyonight.nvim",
-    priority = 800,
-    config = function()
-      require("tokyonight").setup({
-        transparent = false,
-        styles = {
-          sidebars = "dark",
-          floats = "dark",
-        },
-      })
-    end
-  },
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 700,
-    config = function()
-      require("rose-pine").setup({
-        disable_background = false,
-        disable_float_background = false,
-      })
-    end
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    priority = 600,
-    config = function()
-      require("kanagawa").setup({
-        transparent = false,
-        theme = "wave",
-        background = {
-          dark = "wave",
-          light = "lotus"
-        },
-      })
-    end
-  },
-  {
-    "EdenEast/nightfox.nvim",
-    priority = 500,
-    config = function()
-      require("nightfox").setup({
-        options = {
-          transparent = false,
-          styles = {
-            comments = "italic",
-            keywords = "bold",
-            types = "italic,bold",
-          }
-        }
-      })
-    end
-  },
-  {
-    "EdenEast/nightfox.nvim",
-    name = "carbonfox",
-    priority = 400,
-    config = function()
-      require("nightfox").setup({
-        options = {
-          transparent = false,
-          styles = {
-            comments = "italic",
-            keywords = "bold",
-            types = "italic,bold",
-          }
-        }
-      })
-    end
-  }
 }
