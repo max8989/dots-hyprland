2aad218286f376bd08334508d3e09a2b4b032508
Maxime Gagne <max.8989@hotmail.com>
2025-10-05 19:46:14 -0400

    update terminal text

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -14,6 +14,29 @@ return { { "scottmckendry/cyberdream.nvim",
             -- Make vertical split separators visible
             VertSplit = { fg = "#3c4048", bg = "NONE" },
             WinSeparator = { fg = "#3c4048", bg = "NONE" },
+
+            -- Improve text visibility
+            Normal = { fg = "#ffffff", bg = "NONE" },
+            NormalFloat = { fg = "#ffffff", bg = "#1e2124" },
+            FloatBorder = { fg = "#5ea1ff", bg = "#1e2124" },
+
+            -- Terminal colors
+            TermCursor = { fg = "#16181a", bg = "#ffffff" },
+            TermCursorNC = { fg = "#16181a", bg = "#ffffff" },
+
+            -- Make comments more visible
+            Comment = { fg = "#7b8496", italic = true },
+
+            -- String colors
+            String = { fg = "#a1d9a5" },
+
+            -- Function and identifier colors
+            Function = { fg = "#5ea1ff" },
+            Identifier = { fg = "#ffffff" },
+
+            -- Make line numbers more visible
+            LineNr = { fg = "#7b8496" },
+            CursorLineNr = { fg = "#5ea1ff", bold = true },
           }
         },
         -- Disable or enable colorscheme extensions
@@ -29,6 +52,29 @@ return { { "scottmckendry/cyberdream.nvim",
         },
       })
       vim.cmd.colorscheme "cyberdream"
+
+      -- Additional terminal color overrides
+      vim.api.nvim_set_hl(0, 'Terminal', { fg = '#ffffff', bg = 'NONE' })
+
+      -- Override terminal ANSI colors for better visibility
+      vim.g.terminal_color_0 = '#3c4048'  -- black (brighter)
+      vim.g.terminal_color_1 = '#ff6e5e'  -- red
+      vim.g.terminal_color_2 = '#5ef1ff'  -- green
+      vim.g.terminal_color_3 = '#f1ff5e'  -- yellow
+      vim.g.terminal_color_4 = '#5ea1ff'  -- blue
+      vim.g.terminal_color_5 = '#bd5eff'  -- magenta
+      vim.g.terminal_color_6 = '#5ef1ff'  -- cyan
+      vim.g.terminal_color_7 = '#ffffff'  -- white (brighter)
+
+      -- Bright variants
+      vim.g.terminal_color_8 = '#7b8496'   -- bright black
+      vim.g.terminal_color_9 = '#ff6e5e'   -- bright red
+      vim.g.terminal_color_10 = '#5ef1ff'  -- bright green
+      vim.g.terminal_color_11 = '#f1ff5e'  -- bright yellow
+      vim.g.terminal_color_12 = '#5ea1ff'  -- bright blue
+      vim.g.terminal_color_13 = '#bd5eff'  -- bright magenta
+      vim.g.terminal_color_14 = '#5ef1ff'  -- bright cyan
+      vim.g.terminal_color_15 = '#ffffff'  -- bright white
     end,
   },
 }
