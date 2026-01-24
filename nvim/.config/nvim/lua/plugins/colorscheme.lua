253e2bb4c822f801b5757e3809b1eae0314efdd5
Maxime Gagne <max.8989@hotmail.com>
2026-01-23 22:59:56 -0500

    change color scheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,46 +1,11 @@
 return {
   {
-    "Mofiqul/dracula.nvim",
+    "nyoom-engineering/oxocarbon.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require("dracula").setup({
-        colors = {
-          bg = "#282A36",
-          fg = "#F8F8F2",
-          selection = "#44475A",
-          comment = "#6272A4",
-          red = "#FF5555",
-          orange = "#FFB86C",
-          yellow = "#F1FA8C",
-          green = "#50FA7B",
-          purple = "#BD93F9",
-          cyan = "#8BE9FD",
-          pink = "#FF79C6",
-          bright_red = "#FF6E6E",
-          bright_green = "#69FF94",
-          bright_yellow = "#FFFFA5",
-          bright_blue = "#D6ACFF",
-          bright_magenta = "#FF92DF",
-          bright_cyan = "#A4FFFF",
-          bright_white = "#FFFFFF",
-          menu = "#21222C",
-          visual = "#3E4452",
-          gutter_fg = "#4B5263",
-          nontext = "#3B4048",
-        },
-        show_end_of_buffer = false,
-        transparent_bg = false,
-        italic_comment = true,
-        overrides = {
-          ["@keyword"] = { italic = true },
-          ["@function"] = { bold = true },
-          ["@function.method"] = { italic = true },
-          ["@function.method.call"] = { bold = true },
-          Type = { bold = true, italic = false },
-        },
-      })
-      vim.cmd.colorscheme("dracula")
+      vim.opt.background = "dark"
+      vim.cmd.colorscheme("oxocarbon")
     end,
   },
 }
