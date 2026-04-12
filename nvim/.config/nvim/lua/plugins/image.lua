0d4d7678861d4a41972ee16c6848ffcefc9220dc
Maxime Gagne <max.8989@hotmail.com>
2026-04-12 13:41:25 -0400

    update threesiter

--- nvim/.config/nvim/lua/plugins/image.lua
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -2,16 +2,7 @@ return {
   "3rd/image.nvim",
   event = "VeryLazy",
   dependencies = {
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function()
-        require("nvim-treesitter.configs").setup({
-          ensure_installed = { "markdown" },
-          highlight = { enable = true },
-        })
-      end,
-    },
+    "nvim-treesitter/nvim-treesitter",
   },
   opts = {
     backend = "kitty",
